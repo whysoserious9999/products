ace43d401773e00a9b546195005c32c27da6dbbf
Amber Hsiao <cs8430s1001215@gmail.com>
2021-03-24 13:18:09 +0800

    new version

--- products.py
+++ products.py
@@ -14,3 +14,8 @@ print(products) # [[紅茶, 10], [奶茶, 15]]
 
 for p in products:
 	print(p[0], '的價格是: ', p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f: # r-讀取  w-寫入 # utf-8編碼
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
