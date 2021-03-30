d54451b1d814d71fd41c5972f882c845b3d19744
Amber Hsiao <cs8430s1001215@gmail.com>
2021-03-30 11:47:56 +0800

    new version

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 繼續
+		name, price = line.strip().split(',') # strip 清除空格 \n # split 分割 # 直接存入name與price變數中
+		products.append([name, price])
+print(products)
+
+# 使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -12,9 +22,11 @@ while True:
 	products.append([name, price])
 print(products) # [[紅茶, 10], [奶茶, 15]]
 
+# 印出資料
 for p in products:
 	print(p[0], '的價格是: ', p[1])
 
+# 寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f: # r-讀取  w-寫入 # utf-8編碼
 	f.write('商品,價格\n')
 	for p in products:
